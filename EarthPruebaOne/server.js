29081a77c22e44238b179a71aaf3f472b6837af9
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2019-05-03 22:17:30 -0500

    eartth engine

--- EarthPruebaOne/server.js
+++ EarthPruebaOne/server.js
@@ -1,6 +1,10 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
+var ee = require('@google/earthengine');
+var privateKey = require('./privatekey.json');
+ee.data.authenticateViaPrivateKey(privateKey);
+//ee.initialize();
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
